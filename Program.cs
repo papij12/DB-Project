557f37f45671c116186f355188a14ac2a5ad2f3b
irene iradukunda <iradukunda.irene200@gmail.com>
2021-03-04 13:57:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Npgsql;
+using System;
+
+namespace SqlDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1;User Id=postgres;Password=pwd;Database=rental;");
+            conn.Open();
+
+            NpgsqlCommand cmd = new NpgsqlCommand("Select title From movies", conn);
+            NpgsqlDataReader dataReader = cmd.ExecuteReader();
+            while (dataReader.Read())
+            {
+                Console.WriteLine($"Title: {dataReader[0]}");
+            }
+
+            conn.Close();
+        }
+    }
+}
