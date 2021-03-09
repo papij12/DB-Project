17a8df39bbe2be5a4ab5bbfaee9386938c34c6b0
irene iradukunda <iradukunda.irene200@gmail.com>
2021-03-09 21:42:58 +0100

    Added few lines to make the project accept data from the user and by using where clause manipulate the query

--- Program.cs
+++ Program.cs
@@ -7,11 +7,14 @@ namespace SqlDatabase
     {
         static void Main(string[] args)
         {
-
-            NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1;User Id=postgres;Password=pwd;Database=rental;");
+            string connection_path = "Server = 127.0.0.1; User Id = postgres; Password = pwd; Database = rental;";
+            Console.WriteLine("Enter the the movie_Id for your movie");
+            string movie_id = Console.ReadLine();
+            NpgsqlConnection conn = new NpgsqlConnection(connection_path);
             conn.Open();
 
-            NpgsqlCommand cmd = new NpgsqlCommand("Select title From movies", conn);
+            NpgsqlCommand cmd = new NpgsqlCommand("Select title From movies Where movie_id = @movie_id", conn);
+            cmd.Parameters.AddWithValue("@movie_id", int.Parse(movie_id));
             NpgsqlDataReader dataReader = cmd.ExecuteReader();
             while (dataReader.Read())
             {
