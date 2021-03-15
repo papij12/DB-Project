a39d19093a70e08eea8a68bc8548319e5cd4476c
irene iradukunda <iradukunda.irene200@gmail.com>
2021-03-15 20:52:41 +0100

    querries for displaying all actors starring in a movie

--- Program.cs
+++ Program.cs
@@ -10,15 +10,32 @@ namespace SqlDatabase
             string connection_path = "Server = 127.0.0.1; User Id = postgres; Password = pwd; Database = rental;";
             Console.WriteLine("Enter the the movie_Id for your movie");
             string movie_id = Console.ReadLine();
-            NpgsqlConnection conn = new NpgsqlConnection(connection_path);
-            conn.Open();
 
-            NpgsqlCommand cmd = new NpgsqlCommand("Select title From movies Where movie_id = @movie_id", conn);
-            cmd.Parameters.AddWithValue("@movie_id", int.Parse(movie_id));
-            NpgsqlDataReader dataReader = cmd.ExecuteReader();
-            while (dataReader.Read())
+            NpgsqlConnection conn = new NpgsqlConnection(connection_path);
+            //querry displaying details about movies given the movie_id
+            conn.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand("Select title, movie_id, price From movies Where movie_id = @movie_id", conn);
+                
+                    cmd.Parameters.AddWithValue("@movie_id", int.Parse(movie_id));
+                    NpgsqlDataReader dataReader = cmd.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        Console.WriteLine($"movie {dataReader["movie_id"]} has title\"{dataReader["title"]}\"and price : {dataReader["price"]}");
+                    }
+
+            conn.Close();
+
+            // querry for dislpaying all actors starring in a movie 
+            conn.Open();
+         
+            Console.WriteLine("\n first and last name of actors that played in all the movies");
+            Console.WriteLine("--------------------------------------------------------------");
+
+            NpgsqlCommand cmd2 = new NpgsqlCommand("Select  first_name,last_name From actors Join starring On starring.actor_id = actors.actor_id", conn);
+            NpgsqlDataReader dataReader1 = cmd2.ExecuteReader();
+            while (dataReader1.Read())
             {
-                Console.WriteLine($"Title: {dataReader[0]}");
+                Console.WriteLine($" first name: {dataReader1["first_name"]}\"\tlast name : {dataReader1["last_name"]}");
             }
 
             conn.Close();
