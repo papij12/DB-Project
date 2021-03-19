d51a33aa89baedf40f5da6036fce24b8fb7ec49e
irene iradukunda <iradukunda.irene200@gmail.com>
2021-03-19 19:54:56 +0100

    Modified the query so that it can display only the copies of the movie of  the movie_id provided by the user

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace SqlDatabase
             string movie_id = Console.ReadLine();
 
             NpgsqlConnection conn = new NpgsqlConnection(connection_path);
+            Console.Clear();
             //querry displaying details about movies given the movie_id
             conn.Open();
             NpgsqlCommand cmd = new NpgsqlCommand("Select title, movie_id, price From movies Where movie_id = @movie_id", conn);
@@ -20,7 +21,7 @@ namespace SqlDatabase
                     NpgsqlDataReader dataReader = cmd.ExecuteReader();
                     while (dataReader.Read())
                     {
-                        Console.WriteLine($"movie {dataReader["movie_id"]} has title\"{dataReader["title"]}\"and price : {dataReader["price"]}");
+                        Console.WriteLine($"movie {dataReader["movie_id"]}\" has title : {dataReader["title"]}\" It costs : {dataReader["price"]}");
                     }
 
             conn.Close();
@@ -28,31 +29,33 @@ namespace SqlDatabase
             // querry for displaying  actors starring in a movie which the user has provided the movie-id
             conn.Open();
          
-            Console.WriteLine("\n first and last name of actors that played in all the movies");
+            Console.WriteLine("\n first and last name of actors that played in the movie");
             Console.WriteLine("--------------------------------------------------------------");
 
-            NpgsqlCommand cmd2 = new NpgsqlCommand("Select m. movie_id,title,first_name,last_name,price From movies m Join starring s On m.movie_id = s.movie_id Join actors a On s.actor_id = a.actor_id Where m.movie_id = @movie_id", conn);
+            NpgsqlCommand cmd2 = new NpgsqlCommand("Select title,first_name,last_name From movies m Join starring s On m.movie_id = s.movie_id Join actors a On s.actor_id = a.actor_id Where m.movie_id = @movie_id", conn);
             cmd2.Parameters.AddWithValue("@movie_id", int.Parse(movie_id));
             NpgsqlDataReader dataReader1 = cmd2.ExecuteReader();
             while (dataReader1.Read())
             {
-                Console.WriteLine($" movie {dataReader1["movie_id"]} has title\"{dataReader1["title"]}\"actor first name: {dataReader1["first_name"]}\"\t actor last name : {dataReader1["last_name"]}\"  cost {dataReader1["price"]}");
+                Console.WriteLine($"title : {dataReader1["title"]}\" actor first name : {dataReader1["first_name"]}\" actor last name : {dataReader1["last_name"]}");
             }
 
             conn.Close();
-            // query for displaying all the copies of the movie 
-            //conn.Open();
-            //Console.WriteLine("\n list of all the copies of the movie");
-            //Console.WriteLine("--------------------------------------");
+            //query for displaying all the copies of the movie which id was provided bythe user 
 
-            //NpgsqlCommand cmd3 = new NpgsqlCommand("Select copy_id, title From copies Join movies On movies.movie_id = copies.movie_id", conn);
-            //NpgsqlDataReader dataReader2 = cmd3.ExecuteReader();
-            //while (dataReader2.Read())
-            //{
-            //    Console.WriteLine($"copy_id : {dataReader2["copy_id"]}\"\t title : {dataReader2["title"]}");
-            //}
-            //conn.Close();
-            //// querry to display availability of the copies and names of the clients 
+           conn.Open();
+            Console.WriteLine("\n list of all the copies of the movie");
+            Console.WriteLine("--------------------------------------");
+
+            NpgsqlCommand cmd3 = new NpgsqlCommand("Select title, copy_id From copies Join movies On movies.movie_id = copies.movie_id Where movies.movie_id = @movie_id", conn);
+            cmd3.Parameters.AddWithValue("@movie_id", int.Parse(movie_id));
+            NpgsqlDataReader dataReader2 = cmd3.ExecuteReader();
+            while (dataReader2.Read())
+            {
+                Console.WriteLine($"title : {dataReader2["title"]}\" copy-id :{dataReader2["copy_id"]}");
+            }
+            conn.Close();
+            // querry to display availability of the copies and names of the clients 
             //conn.Open();
             //Console.WriteLine("availability of the copies and names of the clients");
             //Console.WriteLine("---------------------------------------------------");
@@ -68,9 +71,9 @@ namespace SqlDatabase
             //Console.WriteLine("Enter the title of the movie");
             //string title = Console.ReadLine();
             //Console.WriteLine("Enter the age restriction of the movie");
-            //int age =int.Parse( Console.ReadLine());
+            //int age = int.Parse(Console.ReadLine());
             //Console.WriteLine("Enter year of production");
-            //int year = int.Parse(Console.ReadLine()); 
+            //int year = int.Parse(Console.ReadLine());
             //Console.WriteLine("Enter price of the movie");
             //int price = int.Parse(Console.ReadLine());
             //NpgsqlCommand cmd5 = new NpgsqlCommand("INSERT INTO movies(movie_id, title, year, age_restriction, price) VALUES(@id, @title, @year, @age, @price)", conn);
